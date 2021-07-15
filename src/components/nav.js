101c078ae6d5f84d9f8d53be8fdf4d3e645c3186
Hyemin Lee <lhmin0614@gmail.com>
2021-07-15 12:15:01 +0900

    navigation

---
+++ src/components/nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <div className="Navigtion">
+      <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navigation;
