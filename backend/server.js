e01be40a72ba4c70a2876466a7c2219f2322645d
Toby <rlaxodntttt@kaist.ac.kr>
2021-07-15 20:59:36 +0900

    add DB in server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,32 @@
 var express = require('express');
-var app = express();
-var server = app.listen(3001, function(){
-    console.log("Express server has started on port 3001")
-})
+const path = require("path");
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 8080;
+
+mongoose.connect("mongodb://localhost:27017/kkamitory", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.once("open", function() {
+  console.log("DB connected!");
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.get('/', (req, res) => {
+    res.status(418).send("Project Kkamitory");
+});
 
 app.get('/', function(req, res){
     res.send('Hello World');
 });
+
+app.listen(port, () => {
+    console.log(port+"에서 express 실행 중");
+})
