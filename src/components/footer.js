b000fcca5639ee00ba1a19a844c45e0290b3fdc5
Hyemin Lee <lhmin0614@gmail.com>
2021-07-21 00:03:49 +0900

    final

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,14 +8,14 @@ class Footer extends Component {
     return (
          <div className = "footer_wraper">
             <div className="footer_bar">
-                <div className = "footer_item"><div className= "footer_text">김태우</div></div>
-                <div className = "footer_item"><div className= "footer_text">이혜민</div></div>
-                <div className = "footer_item"><div className= "footer_text">김태우</div></div>
-                <div className = "footer_item"><div className= "footer_text">이혜민</div></div>
-                <div className = "footer_item"><div className= "footer_text">김태우</div></div>
-                <div className = "footer_item"><div className= "footer_text">이혜민</div></div>
-                <div className = "footer_item"><div className= "footer_text">김태우</div></div>
-                <div className = "footer_item"><div className= "footer_text">이혜민</div></div>
+                <div className = "footer_item"><div className= "footer_text">TAEWOO</div></div>
+                <div className = "footer_item"><div className= "footer_text">HYEMIN</div></div>
+                <div className = "footer_item"><div className= "footer_text">TAEWOO</div></div>
+                <div className = "footer_item"><div className= "footer_text">HYEMIN</div></div>
+                <div className = "footer_item"><div className= "footer_text">TAEWOO</div></div>
+                <div className = "footer_item"><div className= "footer_text">HYEMIN</div></div>
+                <div className = "footer_item"><div className= "footer_text">TAEWOO</div></div>
+                <div className = "footer_item"><div className= "footer_text">HYEMIN</div></div>
             </div>
          </div>
       
